3d991796dc2092cdd84def6cf678e8876fd6f310
/inayak Dwarkoba Gaikwad <vinayakdwarkobagaikwad@gmail.com>
2023-03-05 06:35:11 +0000

    images

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from "../components/Home/Navbar";
 import { useNavigate } from "react-router-dom";
-import Particles, { ISourceOptions } from "./react-tsparticles";
-import { loadFull } from "tsparticles";
-import myImage from './';
+
+import myImage from './bg-mcoe.jpg';
+import logo from "./finallogo20.png";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -24,7 +24,7 @@ const Home = () => {
 
                     <div className="bg-auto bg-no-repeat bg-center" style="background-image: bg-mcoe.jpg"></div>
 
-                    <img src="./bg-mcoe.jpg" alt="mcoe" />
+                    <img src={myImage} alt="mcoe" />
                     <br /><br />
                     <p class="break-after-column snap-right text-white"><p className="text-2xl">ABOUT IT DEPARTMENT</p><br /><br />P.E.S's Modern College of Engineering in Pune established its IT department in 2006 with 60 students, which increased to 120 by 2011-12. The department provides modern technical education with innovative teaching methods and inputs from practicing corporates. It encourages individual initiative, leadership, and adoption of new technology through regular seminars, workshops, and short-term courses. The IT department aims to develop analytical skills, provide academic excellence, promote a multi-dimensional approach, and impart ethical and moral values.</p>
 
